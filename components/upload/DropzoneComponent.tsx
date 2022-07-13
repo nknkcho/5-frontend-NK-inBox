4a2e96c251a4b0a48d9ee93c011a5d1dd05f216b
nknkcho <srirachacho@gmail.com>
2022-07-14 03:16:55 +0900

    s3 연결 시도 이슈: 환경 변수가 적용되지 않는 문제, 파일명 암호화 작업 필요

--- components/upload/DropzoneComponent.tsx
+++ components/upload/DropzoneComponent.tsx
@@ -1,11 +1,54 @@
-import { useCallback } from "react";
+import { useState, useCallback } from "react";
 import { useDropzone } from "react-dropzone";
+import AWS from "aws-sdk";
+import sha256 from "crypto-js/sha256";
 import styles from "./DropzoneComponent.module.scss";
 
 export default function DropzoneComponent() {
-  const onDrop = useCallback((acceptedFiles: any) => {
-    console.log(acceptedFiles);
-  }, []);
+  const [progress, setProgress] = useState(0);
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  console.log(`env : ${process.env.REGION}`);
+  AWS.config.update({
+    accessKeyId: process.env.ACCESS_KEY,
+    secretAccessKey: process.env.SECRET_KEY,
+  });
+
+  const myBucket = new AWS.S3({
+    params: { Bucket: process.env.BUCKET_NAME },
+    region: process.env.REGION,
+  });
+
+  const onDrop = ((acceptedFiles: any) => {
+    setProgress(0);
+    setSelectedFile(acceptedFiles[0]);
+  });
+
+
+  const uploadToBucket = (selectedFile) => {
+    if(selectedFile === null || undefined){
+      return
+    }
+    const params = {
+      ACL: "public-read",
+      Body: selectedFile,
+      Bucket: process.env.BUCKET_NAME,
+      Key: "upload/" + sha256(selectedFile.name).toString(),
+    };
+
+    myBucket.putObject(params)
+    .on("httpUploadProgress", (event) => {
+      setProgress(Math.round((event.loaded / event.total) * 100))
+    }).send((error) => {
+      if(error) console.log(error);
+    });
+  };
+
+  if(selectedFile !== null || undefined){
+    uploadToBucket(selectedFile);
+  }
+  console.log(selectedFile);
+
   const {
     acceptedFiles,
     getRootProps,
@@ -20,11 +63,12 @@ export default function DropzoneComponent() {
     },
   });
 
-  const files = acceptedFiles.map((file: any) => (
-    <li key={file.path}>
-      {file.path} - {file.size} bytes
-    </li>
-  ));
+  // FIXME: type 설정 문제
+  // const files = acceptedFiles.map((file: any) => (
+  //   <li key={file.path}>
+  //     {file.path} - {file.size} bytes
+  //   </li>
+  // ));
 
   return (
     <section>
@@ -36,28 +80,30 @@ export default function DropzoneComponent() {
         })}
       >
         <input {...getInputProps()} />
-        {isDragActive ? isDragAccept ? (
-          <>
-            <div className={styles.dropzoneHeadTitle}>You can upload!</div>
-            <div className={styles.dropzoneSubTitle}>Drop files here.</div>
-          </>
-        ) : (
-          <>
-            <div className={styles.dropzoneHeadTitle}>File type is not allowed</div>
-            <div className={styles.dropzoneSubTitle}>Please upload another file.</div>
-          </>
+        <div>{progress}</div>
+        {isDragActive ? (
+          isDragAccept ? (
+            <>
+              <div className={styles.dropzoneHeadTitle}>You can upload!</div>
+              <div className={styles.dropzoneSubTitle}>Drop files here.</div>
+            </>
+          ) : (
+            <>
+              <div className={styles.dropzoneHeadTitle}>
+                File type is not allowed
+              </div>
+              <div className={styles.dropzoneSubTitle}>
+                Please upload another file.
+              </div>
+            </>
+          )
         ) : (
           <>
             <div className={styles.dropzoneHeadTitle}>File Upload</div>
-            <div className={styles.dropzoneSubTitle}>
-            Drop files here.
-            </div>
+            <div className={styles.dropzoneSubTitle}>Drop files here.</div>
           </>
         )}
       </div>
-      <aside>
-        <ul>{files}</ul>
-      </aside>
     </section>
   );
 }
