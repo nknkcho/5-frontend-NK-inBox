2ffe0b38e82f1f1b3961a51d6aa347ff93c52b55
nknkcho <srirachacho@gmail.com>
2022-07-18 04:27:37 +0900

    gitStaticProps 도입하였으나, 빌드 시 경로 문제 발생

--- pages/portfolios/index.tsx
+++ pages/portfolios/index.tsx
@@ -1,14 +1,39 @@
 import Header from "../../components/UI/Header";
 import PortfolioContent from "../../components/portfolio/PortfolioContent";
+import { getRequest } from "../../utils/fetchData";
 import styles from "./portfolios.module.scss";
+import { InferGetStaticPropsType } from "next";
 
-export default function DetailPage() {
+type Content = {
+  message : string,
+	range : string,
+	title : string,
+	fileName : string,
+	extension : string,
+	portfolioDate : string,
+	about : string,
+	email : string,
+	createdDate : string,
+}
+
+export default function DetailPage({ content }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <div className={styles.portfolioContainer}>
       <Header />
       <div className={styles.alignComponents}>
-        <PortfolioContent/>
+        <PortfolioContent content={content}/>
       </div>
     </div>
   );
+}
+
+export async function getStaticProps() {
+	const res = await getRequest("portfolios/file");
+  const content: Content[] = await res.json();
+
+	return {
+		props: {
+			content
+		}
+	}
 }
