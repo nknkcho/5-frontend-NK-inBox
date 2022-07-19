bd3152480f8e3d6553e62bf5f0f082b0b3b7919c
nknkcho <srirachacho@gmail.com>
2022-07-20 03:14:00 +0900

    fetch한 포트폴리오 데이터 화면에 뿌리기, 애니메이션 적용

--- components/portfolio/PortfolioContent.tsx
+++ components/portfolio/PortfolioContent.tsx
@@ -1,24 +1,55 @@
-import { useEffect, useState, MouseEventHandler } from "react";
+import { useEffect, useState } from "react";
 import Image from "next/image";
 import styles from "../portfolio/PortfolioContent.module.scss";
 import filter from "../../utils/filters";
-import { dummy } from "../../utils/portfolioDummy";
+import { getRequest } from "../../utils/fetchData";
 
-export default function PortfolioFooter() {
-  const data = dummy.filter((element) => {
-    if (element.id === 1) {
-      return true;
-    }
-  });
+type Content = {
+  message: string;
+  range: string;
+  title: string;
+  fileName: string;
+  extension: string;
+  portfolioDate: string;
+  about: string;
+  email: string;
+  createdDate: string;
+};
+
+export default function PortfolioFooter(props: { content: Content }) {
+  const videoPath = (fileName: string, extension: string) => {
+    const path = `https://inbox-portfolio.s3.ap-northeast-2.amazonaws.com/${fileName}.${extension}`;
+    return path;
+  };
+  const portfolioData = props.content;
+  const [videoSrc, setVideoSrc] = useState(
+    videoPath(portfolioData.fileName, portfolioData.extension)
+  );
+  const [videoType, setVideoType] = useState(
+    `video/${portfolioData.extension}`
+  );
+  const [title, setTitle] = useState(portfolioData.title);
+  const [date, setDate] = useState(portfolioData.portfolioDate);
+  const [about, setAbout] = useState(portfolioData.about);
   const [showImg, setShowImg] = useState(true);
   const [changeImg, setChangeImg] = useState(false);
-  const onclickHandler = () => {
+  const onclickHandler = async () => {
     setChangeImg((current) => !current);
+    const res = await getRequest("portfolios/file");
+    const newData = await res.json();
+    console.log(newData);
+    const newPath = videoPath(newData.fileName, newData.extension);
+    setVideoSrc(newPath);
+    setVideoType(`video/${newData.extension}`);
+    setTitle(newData.title);
+    setDate(newData.portfolioDate);
+    setAbout(newData.about);
   };
   const portfolioFilter = filter[Math.floor(Math.random() * filter.length)];
+
   useEffect(() => {
     const selectedFilter = localStorage.getItem("filter");
-    if (selectedFilter === "ON") {
+    if (selectedFilter === "on") {
       setShowImg(true);
     } else {
       setShowImg(false);
@@ -29,28 +60,34 @@ export default function PortfolioFooter() {
     <>
       <div className={styles.portfolioFilter}>
         {showImg && (
-          <div>
+          <div className={ changeImg ? styles.fadeInClass : ""}>
             <Image
               alt="Inbox filter for portfolios"
               src={portfolioFilter}
               layout="responsive"
+              width={100}
+              height={39}
             />
           </div>
         )}
+        {!showImg && (
+          <video className={styles.video} key={videoSrc} controls>
+            <source src={videoSrc} type={videoType} />
+          </video>
+        )}
       </div>
       <div className={styles.footer}>
-        <div className={styles.footerItem}>
-          <span className={styles.description}>{data[0].title}</span>
+        <div className={`${styles.footerItem} ${styles.footerTitle}`}>
+          <span className={styles.description}>{title}</span>
         </div>
         <div className={styles.footerItem}>
           <button className={styles.nextBtn} onClick={onclickHandler}>
             Go to other Project
           </button>
         </div>
-        <div className={styles.footerItem}>
-          <span className={styles.description}>
-            {data[0].date} | {data[0].about}
-          </span>
+        <div className={`${styles.footerItem} ${styles.footerDescription}`}>
+          <p className={styles.description}>{date}</p>
+          <p className={styles.description}>{about}</p>
         </div>
       </div>
     </>
