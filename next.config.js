26188348b35d1186f48fbb0ab7685047a3fae6f0
nknkcho <srirachacho@gmail.com>
2022-07-28 23:12:46 +0900

    next-seo 라이브러리 설치에 따른 업데이트

--- next.config.js
+++ next.config.js
@@ -6,6 +6,6 @@ const nextConfig = {
 module.exports = {
   nextConfig,
   images: {
-    deviceSizes: [600, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    deviceSizes: [600, 640, 750, 828, 1080, 1200, 1400, 1920, 2048, 3840],
   }
 }
