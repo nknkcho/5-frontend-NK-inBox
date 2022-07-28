a101c96044c5f3d14f9864d69cfcdef004d76fbf
nknkcho <srirachacho@gmail.com>
2022-07-29 01:02:41 +0900

    구글 서치콘솔 연결을 위한 meta 태그 연결

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,7 @@ class MyDocument extends Document {
     return (
       <Html lang="ko">
         <Head>
+        <meta name="google-site-verification" content="NVTcM-puRVWp6zK2sa1lrfnyw1S6SbGob4iEipkUO6w" />
           <link />
         </Head>
         <body>
