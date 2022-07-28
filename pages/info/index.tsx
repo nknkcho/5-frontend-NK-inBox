d96ab16d3bf6827f448f690dacfeac86cf2b33f5
nknkcho <srirachacho@gmail.com>
2022-07-28 23:14:30 +0900

    페이지별 SEO 설정

--- pages/info/index.tsx
+++ pages/info/index.tsx
@@ -1,13 +1,33 @@
 import dynamic from "next/dynamic";
 import Header from "../../components/UI/Header";
 import styles from "../../styles/info.module.scss";
+import metadata from "../../components/metadata";
+import { NextSeo } from "next-seo";
 
-const Category = dynamic((): Promise<any> => import("../../components/setting/SettingList").then((module) => module.Category), {ssr: false})
-const GoBackButton = dynamic((): Promise<any> => import("../../components/setting/SettingList").then((module) => module.GoBackButton), {ssr: false})
+const Category = dynamic(
+  (): Promise<any> =>
+    import("../../components/setting/SettingList").then(
+      (module) => module.Category
+    ),
+  { ssr: false }
+);
+const GoBackButton = dynamic(
+  (): Promise<any> =>
+    import("../../components/setting/SettingList").then(
+      (module) => module.GoBackButton
+    ),
+  { ssr: false }
+);
 
 export default function Intro() {
   return (
-    <div>
+    <>
+      <NextSeo
+        title="Info"
+        canonical={`${metadata.meta.url}/info`}
+        openGraph={{ url: `${metadata.meta.url}/info` }}
+      />
+
       <Header />
       <div className={styles.infoContainer}>
         <p className={styles.infoText}>
@@ -25,6 +45,6 @@ export default function Intro() {
           <GoBackButton />
         </div>
       </div>
-    </div>
+    </>
   );
 }
