d76d0068c2d5b0f7a10ca62ea61d92ef2885d720
nknkcho <srirachacho@gmail.com>
2022-08-02 00:14:57 +0900

    필터 이미지 컴포넌트의 중복 코드 리팩토링

--- components/portfolio/PortfolioContent.tsx
+++ components/portfolio/PortfolioContent.tsx
@@ -1,114 +1,109 @@
-import { useEffect, useState } from "react";
-import Image from "next/image";
-import styles from "../portfolio/PortfolioContent.module.scss";
-import filter from "../../utils/filters";
-import { getRequest } from "../../utils/fetchData";
+import { useEffect, useState } from 'react'
+import Image from 'next/image'
+import styles from '../portfolio/PortfolioContent.module.scss'
+import filter from '../../utils/filters'
+import { getRequest } from '../../utils/fetchData'
+
+type FilterImageProps = {
+  animation: boolean,
+  portfolioFilter: string | StaticImport,
+}
+
+export const FilterImage = (props : FilterImageProps) => {
+  return (
+    <div
+            className={
+              props.animation ? `${styles.imgWrapper} ${styles.fadeInClass}` : `${styles.imgWrapper}`
+            }
+          >
+            <Image
+              alt="Inbox filter for portfolios"
+              src={props.portfolioFilter}
+              layout="fill"
+              objectFit="cover"
+              priority
+            />
+          </div>
+  )
+}
 
 type Content = {
-  message: string;
-  range: string;
-  title: string;
-  fileName: string;
-  extension: string;
-  portfolioDate: string;
-  about: string;
-  email: string;
-  createdDate: string;
-};
+  message: string
+  range: string
+  title: string
+  fileName: string
+  extension: string
+  portfolioDate: string
+  about: string
+  email: string
+  createdDate: string
+}
 
 export default function PortfolioFooter(props: { content: Content }) {
   // 영상의 경로를 return하는 함수
   const videoPath = (fileName: string, extension: string) => {
-    const path = `https://inbox-portfolio.s3.ap-northeast-2.amazonaws.com/${fileName}.${extension}`;
-    return path;
-  };
+    const path = `https://inbox-portfolio.s3.ap-northeast-2.amazonaws.com/${fileName}.${extension}`
+    return path
+  }
   // 페이지 첫 로딩 시, 받아오는 데이터를 받아오는 변수
-  const portfolioData = props.content;
+  const portfolioData = props.content
   // 포트폴리오 비디오 source의 상태
   const [videoSrc, setVideoSrc] = useState(
     videoPath(portfolioData.fileName, portfolioData.extension)
-  );
+  )
   // 포트폴리오 비디오 확장자의 상태
-  const [videoType, setVideoType] = useState(
-    `video/${portfolioData.extension}`
-  );
+  const [videoType, setVideoType] = useState(`video/${portfolioData.extension}`)
   // 포트폴리오 제목의 상태
-  const [title, setTitle] = useState(portfolioData.title);
+  const [title, setTitle] = useState(portfolioData.title)
   // 포트폴리오 업로드일의 상태
-  const [date, setDate] = useState(portfolioData.portfolioDate);
+  const [date, setDate] = useState(portfolioData.portfolioDate)
   // 포트폴리오 설명의 상태
-  const [about, setAbout] = useState(portfolioData.about);
+  const [about, setAbout] = useState(portfolioData.about)
   // setting 설정에 따라 filter 이미지를 보여줄 것인지를 결정하는 상태
-  const [showImg, setShowImg] = useState(true);
+  const [showImg, setShowImg] = useState(true)
   // 포트폴리오 전환 버튼 클릭 시 발생하는 이벤트
   const onclickHandler = async () => {
     //await getRequest(`cookies`);
-    const res = await getRequest("portfolios/file");
-    const newData = await res.json();
-    const newPath = videoPath(newData.fileName, newData.extension);
-    setVideoSrc(newPath);
-    setVideoType(`video/${newData.extension}`);
-    setTitle(newData.title);
-    setDate(newData.portfolioDate);
-    setAbout(newData.about);
-    setAnimation((current) => !current);
-  };
+    const res = await getRequest('portfolios/file')
+    const newData = await res.json()
+    const newPath = videoPath(newData.fileName, newData.extension)
+    setVideoSrc(newPath)
+    setVideoType(`video/${newData.extension}`)
+    setTitle(newData.title)
+    setDate(newData.portfolioDate)
+    setAbout(newData.about)
+    setAnimation(current => !current)
+  }
 
   // 필터 이미지 불러오기
-  const portfolioFilter = filter[Math.floor(Math.random() * filter.length)];
+  const portfolioFilter = filter[Math.floor(Math.random() * filter.length)]
   // 이미지 애니메이션 변경을 위한 상태값
-  const [animation, setAnimation] = useState(false);
+  const [animation, setAnimation] = useState(false)
 
   // 페이지 로딩 시, filter 세팅값에 따라 이미지를 보여줄 것인지에 따라 상태값 업데이트
   useEffect(() => {
-    const selectedFilter = localStorage.getItem("filter");
-    if (selectedFilter === "on") {
-      setShowImg(true);
+    const selectedFilter = localStorage.getItem('filter')
+    if (selectedFilter === 'on') {
+      setShowImg(true)
     } else {
-      setShowImg(false);
+      setShowImg(false)
     }
-  }, [showImg]);
+  }, [showImg])
 
   return (
     <>
       <div className={styles.portfolioFilter}>
         {showImg && animation && (
-          <>
-            <div
-              className={
-                animation
-                  ? `${styles.imgWrapper} ${styles.fadeInClass}`
-                  : `${styles.imgWrapper}`
-              }
-            >
-              <Image
-                alt="Inbox filter for portfolios"
-                src={portfolioFilter}
-                layout="fill"
-                objectFit="cover"
-                priority
-              />
-            </div>
-          </>
+          <FilterImage
+          animation={animation}
+          portfolioFilter={portfolioFilter}
+          />
         )}
         {showImg && !animation && (
-          <>
-            <div
-              className={
-                animation
-                  ? `${styles.imgWrapper}`
-                  : `${styles.imgWrapper} ${styles.fadeInClass}`
-              }
-            >
-              <Image
-                alt="Inbox filter for portfolios"
-                src={portfolioFilter}
-                layout="fill"
-                objectFit="cover"
-                priority
-              />
-            </div>
-          </>
+          <FilterImage
+          animation={!animation}
+          portfolioFilter={portfolioFilter}
+          />
         )}
         {!showImg && (
           <video className={styles.video} key={videoSrc} controls>
@@ -131,5 +126,5 @@ export default function PortfolioFooter(props: { content: Content }) {
         </div>
       </div>
     </>
-  );
+  )
 }
