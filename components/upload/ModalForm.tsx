de574812a186b8366810a47dd07bccbc0e61db36
nknkcho <srirachacho@gmail.com>
2022-08-06 23:20:58 +0900

    file drag&drop 부분 조건문에 따른 컴포넌트 변경 코드 리팩토링

--- components/upload/ModalForm.tsx
+++ components/upload/ModalForm.tsx
@@ -1,94 +1,113 @@
-import { useState } from "react";
-import Image from "next/image";
-import { useDropzone } from "react-dropzone";
-import AWS from "aws-sdk";
-import sha256 from "crypto-js/sha256";
-import styles from "./ModalForm.module.scss";
-import closePic from "../../public/close.svg";
-import FormList from "./ModalFormList";
+import { useState } from 'react'
+import Image from 'next/image'
+import { useDropzone } from 'react-dropzone'
+import AWS from 'aws-sdk'
+import sha256 from 'crypto-js/sha256'
+import styles from './ModalForm.module.scss'
+import closePic from '../../public/close.svg'
+import FormList from './ModalFormList'
 
 export default function ModalForm(props: { closeModal: Function }) {
   // 파일 업로드 진행 상황을 확인할 수 있는 progress bar를 위한 State
-  const [progress, setProgress] = useState(0);
+  const [progress, setProgress] = useState(0)
   // 업로드된 파일의 key
-  const [fileKey, setFileKey] = useState("");
+  const [fileKey, setFileKey] = useState('')
 
   // S3 접근을 위한 AWS 계정 설정
   AWS.config.update({
     accessKeyId: process.env.NEXT_PUBLIC_ACCESS_KEY,
     secretAccessKey: process.env.NEXT_PUBLIC_SECRET_KEY,
-  });
+  })
 
   // S3의 버켓 접근
   const s3 = new AWS.S3({
     params: { Bucket: process.env.NEXT_PUBLIC_BUCKET_NAME },
     region: process.env.NEXT_PUBLIC_REGION,
-  });
+  })
 
   // dropzone에 들어온 파일을 받는 함수
   const onDrop = (files: any) => {
-    const acceptedFiles = files[0];
-    setFileKey(
-      `${sha256(acceptedFiles.name).toString()}.${acceptedFiles.type.substring(6)}`
-    );
-    setProgress(0);
-    uploadToBucket(acceptedFiles);
-  };
+    const acceptedFiles = files[0]
+    setFileKey(`${sha256(acceptedFiles.name).toString()}.${acceptedFiles.type.substring(6)}`)
+    setProgress(0)
+    uploadToBucket(acceptedFiles)
+  }
 
   // S3 버켓에 파일을 올리는 함수
   const uploadToBucket = async (
     selectedFile: {
-      type: string;
-      name: string | CryptoJS.lib.WordArray;
+      type: string
+      name: string | CryptoJS.lib.WordArray
     } | null
   ) => {
     if (!selectedFile) {
-      return;
+      return
     }
 
     // 파일 확장자 정보 담기
     const params: any = {
-      ACL: "public-read",
+      ACL: 'public-read',
       Body: selectedFile,
       Bucket: process.env.NEXT_PUBLIC_BUCKET_NAME,
-      Key:
-        `${sha256(selectedFile.name).toString()}.${selectedFile.type.substring(6)}`,
-    };
+      Key: `${sha256(selectedFile.name).toString()}.${selectedFile.type.substring(6)}`,
+    }
 
     // FIXME: params : No overload matches this call. 이라는 에러의 의미?
     s3.putObject(params)
-      .on("httpUploadProgress", (event) => {
-        setProgress(Math.round((event.loaded / event.total) * 100));
+      .on('httpUploadProgress', event => {
+        setProgress(Math.round((event.loaded / event.total) * 100))
       })
-      .send((error) => {
-        if (error) console.log(error);
-      });
-  };
+      .send(error => {
+        if (error) console.log(error)
+      })
+  }
 
-  const {
-    getRootProps,
-    getInputProps,
-    isDragActive,
-    isDragAccept,
-    isDragReject,
-  } = useDropzone({
+  const { getRootProps, getInputProps, isDragActive, isDragAccept, isDragReject } = useDropzone({
     onDrop,
     accept: {
-      "video/mp4": [],
-      "video/avi": [],
-      "video/webm": [],
-      "video/wmv": [],
-      "video/mov": [],
+      'video/mp4': [],
+      'video/avi': [],
+      'video/webm': [],
+      'video/wmv': [],
+      'video/mov': [],
     },
-  });
+  })
+
+  const isDragAcceptHandler = (isDragAccept: boolean) => {
+    if (isDragAccept) {
+      return (
+        <>
+          <div className={styles.dropzoneHeadTitle}>You can upload!</div>
+          <div className={styles.dropzoneSubTitle}>Drop files here.</div>
+        </>
+      )
+    } else {
+      return (
+        <>
+          <div className={styles.dropzoneHeadTitle}>File type is not allowed</div>
+          <div className={styles.dropzoneSubTitle}>Please upload another file.</div>
+        </>
+      )
+    }
+  }
+
+  const isDragActiveHandler = (isDragActive: boolean) => {
+    if (isDragActive) {
+      return isDragAcceptHandler(isDragAccept)
+    } else {
+      return (
+        <>
+          <div className={styles.dropzoneHeadTitle}>File Upload</div>
+          <div className={styles.dropzoneSubTitle}>Drop files here.</div>
+        </>
+      )
+    }
+  }
 
   return (
     <div className={styles.modalBackground}>
       <div className={styles.modalContainer}>
-        <button
-          className={styles.closeBtn}
-          onClick={() => props.closeModal(false)}
-        >
+        <button className={styles.closeBtn} onClick={() => props.closeModal(false)}>
           <Image alt="Close" src={closePic} width={40} height={40} />
         </button>
         <h2 className={styles.modalTitle}>Upload your Box</h2>
@@ -103,34 +122,7 @@ export default function ModalForm(props: { closeModal: Function }) {
               })}
             >
               <input {...getInputProps()} />
-              {isDragActive ? (
-                isDragAccept ? (
-                  <>
-                    <div className={styles.dropzoneHeadTitle}>
-                      You can upload!
-                    </div>
-                    <div className={styles.dropzoneSubTitle}>
-                      Drop files here.
-                    </div>
-                  </>
-                ) : (
-                  <>
-                    <div className={styles.dropzoneHeadTitle}>
-                      File type is not allowed
-                    </div>
-                    <div className={styles.dropzoneSubTitle}>
-                      Please upload another file.
-                    </div>
-                  </>
-                )
-              ) : (
-                <>
-                  <div className={styles.dropzoneHeadTitle}>File Upload</div>
-                  <div className={styles.dropzoneSubTitle}>
-                    Drop files here.
-                  </div>
-                </>
-              )}
+              {isDragActiveHandler(isDragActive)}
               <div className={styles.dropzoneProgress}>{progress}%</div>
             </div>
           </section>
@@ -138,5 +130,5 @@ export default function ModalForm(props: { closeModal: Function }) {
         </form>
       </div>
     </div>
-  );
+  )
 }
