336cd1d0448fd75b29d68755793e540066d6d4bc
nknkcho <srirachacho@gmail.com>
2022-08-22 03:05:12 +0900

    return 필요 없는 fragment 삭제

--- components/UI/FormBox.tsx
+++ components/UI/FormBox.tsx
@@ -1,20 +1,14 @@
-import Link from "next/link";
-import Image from "next/image";
-import boxPic from "../../public/box.svg";
-import styles from "./FormBox.module.scss";
+import Link from 'next/link'
+import Image from 'next/image'
+import boxPic from '../../public/box.svg'
+import styles from './FormBox.module.scss'
 
 export default function FormBox() {
   return (
-    <>
     <div className={styles.homeBox}>
       <Link href={`/portfolios`}>
-        <Image
-          alt="Box icon for homepage"
-          src={boxPic}
-          layout="responsive"
-        />
+        <Image alt="Box icon for homepage" src={boxPic} layout="responsive" />
       </Link>
-      </div>
-    </>
-  );
+    </div>
+  )
 }
