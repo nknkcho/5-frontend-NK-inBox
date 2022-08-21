2342c56f57e2e2adb67ee3af8c7c663f18eb816b
nknkcho <srirachacho@gmail.com>
2022-08-22 03:04:52 +0900

    return 필요 없는 fragment 삭제

--- components/UI/Header.tsx
+++ components/UI/Header.tsx
@@ -1,14 +1,12 @@
-import MenuList from "./HeaderMenu";
-import HeaderBox from "./HeaderBox";
-import styles from "./Header.module.scss";
+import MenuList from './HeaderMenu'
+import HeaderBox from './HeaderBox'
+import styles from './Header.module.scss'
 
 export default function Header() {
   return (
-    <>
-      <div className={styles.header}>
-        <MenuList />
-        <HeaderBox />
-      </div>
-    </>
-  );
+    <div className={styles.header}>
+      <MenuList />
+      <HeaderBox />
+    </div>
+  )
 }
